95442b46128febdcdb5f5a04cfb7104fab69a644
alexSam <alexsamalot19@gmail.com>
2020-02-11 14:54:40 -0500

    User Class Created

--- MealTracker.java
+++ MealTracker.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 public class MealTracker {
 
     public static void main(String args[]){
-        if (args.length == 0){
-            System.out.println("Usage java MealTracker <firstName>");
-            System.err.println("firstName is required");
-            System.exit(1);
+
+            // Since our User class is in the same package, we don't need to import it.
+            User jim = new User("Jim", "test", "jimTest@testmail.com", 99);
+
+            // We can call our public methods...
+            jim.introduce();
+
+            // Here, we're calling a method on the class per se.
+            System.out.println(User.getCount());
+
         }
-
-        User user = new User(args[0]);
-
-        user.welcomeUser();
-    }
 }
--- User.java
+++ User.java
@@ -1,11 +1,64 @@
-public class User {
-    private String firstName;
+import java.util.Scanner;
 
-    public User(String firstName) {
+public class User {
+    private static int sUsersCreated = 0;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int targetAdherence = 90;
+
+    /* This is called a constructor. This is th function that's called whenever
+     *   you create a new User, and is generally where we put our initialization
+     *   logic. */
+    User(String firstName, String lastName, String email, int targetAdherence) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEmail(email);
+        this.setTargetAdherence(targetAdherence);
+
+        // Increment count whenever we create a new user.
+        User.sUsersCreated += 1;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    private void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public void welcomeUser() {
-        System.out.printf("Welcome %s ! %n", firstName);
+    public String getLastName() {
+        return this.lastName;
     }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTargetAdherence() {
+        return this.targetAdherence;
+    }
+
+    public void setTargetAdherence(int targetAdherence) {
+        this.targetAdherence = targetAdherence;
+    }
+
+    public void introduce() {
+        System.out.println(String.format("My username is %s.", this.firstName));
+    }
+
+    // This is a method on the CLASS-not an individual user object.
+    public static int getCount() {
+        return User.sUsersCreated;
+    }
+
 }
